a80b2259b122c52f6b340f0f804816cd30935423
Rowan TL <skrublaub@cognitiveweeds.net>
2024-07-24 16:30:09 -0500

    add gravity

--- simulate.py
+++ simulate.py
@@ -2,8 +2,9 @@ import pybullet as p
 from time import sleep
 
 physicsClient = p.connect(p.GUI)
-
+p.setGravity(0,0,-9.8)
 p.loadSDF("box.sdf")
+
 for _ in range(1000):
     p.stepSimulation()
     sleep((1/70))
