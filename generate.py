7e2f39d2bda86b874c6c2e8269784151d406ae5d
Rowan TL <skrublaub@cognitiveweeds.net>
2024-07-24 20:42:03 -0500

    25 blocks

--- generate.py
+++ generate.py
@@ -6,9 +6,16 @@ z = 0.5
 s = 1  # size for later
 
 pyrosim.Start_SDF("boxes.sdf")
-for n in range(10):
-    pyrosim.Send_Cube(name=f"Box{n}", pos=[x,y,z] , size=[s,s,s])
-    z += 1
-    s *= .9
-
+for n in range(5):
+    for m in range(5):
+        for k in range(10):
+            pyrosim.Send_Cube(name=f"Box{n}{m}{k}", pos=[x,y,z] , size=[s,s,s])
+            z += 1
+            s *= .9
+        z = 0.5
+        s = 1
+        y += 1
+    x += 1
+    y = 0
+    
 pyrosim.End()
