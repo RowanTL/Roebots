74e12ee7209f127aa012508fdba36359ed1ca171
Rowan TL <skrublaub@cognitiveweeds.net>
2024-07-24 20:12:46 -0500

    start on surface

--- simulate.py
+++ simulate.py
@@ -2,13 +2,14 @@ import pybullet as p
 import pybullet_data
 from time import sleep
 
+# on the manylinks section at the moment
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 p.loadSDF("box.sdf")
 
-for _ in range(1000):
+for _ in range(2000):
     p.stepSimulation()
     sleep((1/70))
 
