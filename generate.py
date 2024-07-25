ffe5bc052512c6861dcc118a6c013330337efc17
Rowan TL <skrublaub@cognitiveweeds.net>
2024-07-24 20:18:19 -0500

    next to and above

--- generate.py
+++ generate.py
@@ -2,8 +2,9 @@ import pyrosim.pyrosim as pyrosim
 
 x = 0
 y = 0
-z = 1.5
+z = 0.5
 
 pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[1,2,3])
+pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[1,1,1])
+pyrosim.Send_Cube(name="Box2", pos=[1,0,1.5] , size=[1,1,1])
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -7,7 +7,7 @@ physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("box.sdf")
+p.loadSDF("boxes.sdf")
 
 for _ in range(2000):
     p.stepSimulation()
