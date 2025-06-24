30e2bfb332d461d11d588bd2eadd87395ba6044f
Rowan Torbitzky-Lane <rowan@protonmail.com>
2025-06-24 14:03:27 -0500

    done with saving sensor values

--- simulate.py
+++ simulate.py
@@ -1,6 +1,8 @@
 import pybullet as p
 import pybullet_data
 from time import sleep
+import pyrosim.pyrosim as pyrosim
+import numpy as np
 
 # on the manylinks section at the moment
 physicsClient = p.connect(p.GUI)
@@ -11,8 +13,17 @@ robotId = p.loadURDF("body.urdf")
 planeId = p.loadURDF("plane.urdf")
 p.loadSDF("world.sdf")
 
-for _ in range(2000):
+pyrosim.Prepare_To_Simulate(robotId)
+
+iter_amt: int = 100
+backlegSensorValues = np.zeros(iter_amt)
+
+for n in range(iter_amt):
     p.stepSimulation()
+    # touch sensors only work with non-root links
+    backlegSensorValues[n] = pyrosim.Get_Touch_Sensor_Value_For_Link("Backleg")
     sleep((1/70))
 
+print(backlegSensorValues)
+np.save("data/backlegSensor.npy", backlegSensorValues)
 p.disconnect()
